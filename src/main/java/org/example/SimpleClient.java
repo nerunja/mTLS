345a5dd34ff81636c88511c920ca1b216c6da58c
Nerunjakumar Soubaya <nerunjakumar.soubaya@gmail.com>
2024-09-14 10:05:43 +0530

    initial commit

---
+++ src/main/java/org/example/SimpleClient.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.KeyStore;
+
+// to enable logging add jvm arg -Djavax.net.debug=ssl:handshake
+
+public class SimpleClient {
+    public static void main(String[] args) throws Exception {
+
+        System.setProperty("javax.net.debug", "ssl,handshake");
+        // Load client's key store
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load(new FileInputStream("client_keystore.jks"), "clientpass".toCharArray());
+
+        // Set up key manager factory
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+        keyManagerFactory.init(keyStore, "clientpass".toCharArray());
+
+        // Load trust store
+        KeyStore trustStore = KeyStore.getInstance("JKS");
+        trustStore.load(new FileInputStream("client_truststore.jks"), "clientpass".toCharArray());
+
+        // Set up trust manager factory
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+        trustManagerFactory.init(trustStore);
+
+        // Set up SSL context
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+        // Create SSL socket
+        SSLSocketFactory socketFactory = sslContext.getSocketFactory();
+        SSLSocket socket = (SSLSocket) socketFactory.createSocket("localhost", 8443);
+
+        // Send message to server
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        writer.println("Hello, server!");
+        String response = reader.readLine();
+        System.out.println("Received from server: " + response);
+
+        // Close connections
+        reader.close();
+        writer.close();
+        socket.close();
+    }
+}
---
+++ src/main/java/org/example/SimpleServer.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.KeyStore;
+
+// to enable logging add jvm arg -Djavax.net.debug=ssl:handshake
+
+public class SimpleServer {
+
+    public static void main(String[] args) throws Exception {
+
+        System.setProperty("javax.net.debug", "ssl,handshake");
+        // Load server's key store
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load(new FileInputStream("server_keystore.jks"), "serverpass".toCharArray());
+
+        // Set up key manager factory
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+        keyManagerFactory.init(keyStore, "serverpass".toCharArray());
+
+        // Load trust store
+        KeyStore trustStore = KeyStore.getInstance("JKS");
+        trustStore.load(new FileInputStream("server_truststore.jks"), "serverpass".toCharArray());
+
+        // Set up trust manager factory
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+        trustManagerFactory.init(trustStore);
+
+        // Set up SSL context
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+        // Create SSL server socket
+        SSLServerSocketFactory serverSocketFactory = sslContext.getServerSocketFactory();
+        SSLServerSocket serverSocket = (SSLServerSocket) serverSocketFactory.createServerSocket(8443);
+        serverSocket.setNeedClientAuth(true);
+
+        System.out.println("Server started...");
+
+        // Accept client connection
+        SSLSocket socket = (SSLSocket) serverSocket.accept();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+        // Read and respond to client message
+        String message = reader.readLine();
+        System.out.println("Received: " + message);
+        writer.println("Hello, client!");
+
+        // Close connections
+        reader.close();
+        writer.close();
+        socket.close();
+        serverSocket.close();
+    }
+}
